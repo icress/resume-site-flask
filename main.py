e6c3d893dbd21556759bbf8177fd74e32e21a1fe
icres <icress26@gmail.com>
2023-03-08 10:56:16 -0700

    Updated projects

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    year = datetime.today().year
+    return render_template("index.html", year=year)
 
 
 if __name__ == "__main__":
